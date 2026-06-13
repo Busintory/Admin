ac1e0d28d625adc6b4cb084ed992e124d81c70cc
Great <jgreat770@gmail.com>
2026-06-13 21:30:37 +0100

    Implement PWA update notification and caching improvements

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,33 @@
-const CACHE_NAME = "busintory-admin-v1";
+const CACHE_NAME = "busintory-admin-v2";
 
-const urlsToCache = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css"
+  "./style.css",
+  "./js/app.js"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
